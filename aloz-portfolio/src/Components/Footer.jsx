834c950c400065b3b47088c7325da055910d2edc
ALO <135142990+karlovkso@users.noreply.github.com>
2024-12-27 10:05:37 +0800

    setup

---
+++ aloz-portfolio/src/Components/Footer.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Footer() {
+  return <footer id="Footer">Footer</footer>;
+}
+
+export default Footer;
---
+++ aloz-portfolio/src/Components/Header.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header id="Header">
+      <div className="navbar justify-center text-2xl pt-8">
+        <a href="/" className="font-bold text-base-content">
+          ALOZ
+        </a>
+        <a href="/" className="font-black pl-1 text-accent">
+          .
+        </a>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
