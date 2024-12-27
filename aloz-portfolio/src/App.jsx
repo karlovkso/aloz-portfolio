2b0c511ade4de5af1fc7c5dc71f0516cc0f9fefd
ALO <135142990+karlovkso@users.noreply.github.com>
2024-12-27 17:09:17 +0800

    revise

--- aloz-portfolio/src/App.jsx
+++ aloz-portfolio/src/App.jsx
@@ -5,17 +5,23 @@ import Skills from "./Pages/Skills";
 import Experience from "./Pages/Experience";
 import Projects from "./Pages/Projects";
 import Footer from "./Components/Footer";
+import PageLoader from "./Components/PageLoader.jsx";
+import Particles from "./Components/Particles.jsx";
 import "./App.css";
 
 function App() {
   return (
-    <div>
-      <Header />
-      <Home />
-      <Skills />
-      <Experience />
-      <Projects />
-      <Footer />
+    <div basename="/aloz-portfolio">
+      <PageLoader />
+      <Particles />
+      <div id="page">
+        <Header />
+        <Home />
+        <Skills />
+        <Experience />
+        <Projects />
+        <Footer />
+      </div>
     </div>
   );
 }
--- aloz-portfolio/src/main.jsx
+++ aloz-portfolio/src/main.jsx
@@ -2,15 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import PageLoader from "./Components/PageLoader.jsx";
-import Particles from "./Components/Particles.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode basename="/aloz-portfolio">
-    <PageLoader />
-    <Particles />
-    <div id="page">
-      <App />
-    </div>
+  <StrictMode>
+    <App />
   </StrictMode>
 );
