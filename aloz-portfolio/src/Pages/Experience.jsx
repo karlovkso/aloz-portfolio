e8d108e956c59c9635af474a7f7b6c8878673b1d
Z <karlojames.vequiso@gmail.com>
2025-10-24 09:58:34 +0800

    update oct 2025

--- aloz-portfolio/src/Pages/Experience.jsx
+++ aloz-portfolio/src/Pages/Experience.jsx
@@ -18,25 +18,32 @@ export default function Experience() {
               </span>
               <br />
               <span className="text-start">
-                Polytechnic University of the Philippines - Parañaque Campus
+                Polytechnic University of the Philippines - Parañaque
               </span>
               <br />
               <span className="text-start">
                 Bachelor of Science in Computer Engineering
               </span>
               <br />
-              {/* <button
-                className="hover:text-accent hover:font-semibold transition duration-300"
-              >
-                Magna Cum Laude
-              </button> */}
+              <span className="text-start">Magna Cum Laude</span>
             </p>
           </div>
 
           <div className="divider"></div>
 
-          <div className="font-bold text-3xl md:text-4xl mt-14">
+          <div className="font-bold text-3xl md:text-4xl mt-12">
             <p className="text-start md:text-left">Experience</p>
+          </div>
+          <div className="font-normal text-base md:text-lg mt-5">
+            <p>
+              <span className="text-accent font-semibold">
+                October 2025 – Present
+              </span>
+              <br />
+              <span>Macky Champ Software Ventures Inc.</span>
+              <br />
+              <span>Junior Software Developer</span>
+            </p>
           </div>
           <div className="font-normal text-base md:text-lg mt-5">
             <p>
@@ -71,6 +78,59 @@ export default function Experience() {
               <span>VCT Network and Data Solutions</span>
               <br />
               <span>Software Developer Intern</span>
+            </p>
+          </div>
+
+          <div className="divider mt-14"></div>
+
+          <div className="font-bold text-3xl md:text-4xl mt-12">
+            <p className="text-start md:text-left">Certifications & Awards</p>
+          </div>
+          <div className="font-normal text-base md:text-lg mt-5">
+            <p>
+              <span className="text-accent font-semibold">
+                Software Engineer of the Year
+              </span>
+              <br />
+              <span>October 2025</span>
+              <br />
+              <span>Polytechnic University of the Philippines - Parañaque</span>
+            </p>
+          </div>
+
+          <div className="font-normal text-base md:text-lg mt-5">
+            <p>
+              <span className="text-accent font-semibold">
+                Best Thesis Presenter
+              </span>
+              <br />
+              <span>October 2025</span>
+              <br />
+              <span>Polytechnic University of the Philippines - Parañaque</span>
+            </p>
+          </div>
+
+          <div className="font-normal text-base md:text-lg mt-5">
+            <p>
+              <span className="text-accent font-semibold">
+                Lean Six Sigma White Belt
+              </span>
+              <br />
+              <span>February 2024</span>
+              <br />
+              <span>Six Sigma PH</span>
+            </p>
+          </div>
+
+          <div className="font-normal text-base md:text-lg mt-5">
+            <p>
+              <span className="text-accent font-semibold">
+                Best System Analyst
+              </span>
+              <br />
+              <span>August 2023</span>
+              <br />
+              <span>Polytechnic University of the Philippines - Parañaque</span>
             </p>
           </div>
         </div>
